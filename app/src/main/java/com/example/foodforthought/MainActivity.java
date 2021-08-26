97197b55f1689a6bd6bde336aed36ad418344b88
rfriis <rubenfriis@gmail.com>
2021-08-26 12:51:32 +0100

    Initial Fragment Setup

--- app/src/main/java/com/example/foodforthought/MainActivity.java
+++ app/src/main/java/com/example/foodforthought/MainActivity.java
@@ -1,14 +1,94 @@
 package com.example.foodforthought;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
+    private int currentNavItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
+//            @Override
+//            public boolean onOptionsItemSelected(MenuItem item) {
+//                if (item != null && item.getItemId() == android.R.id.home) {
+//                    if (drawer.isDrawerOpen(GravityCompat.END)) {
+//                        drawer.closeDrawer(GravityCompat.END);
+//                    } else {
+//                        drawer.openDrawer(GravityCompat.END);
+//                    }
+//                }
+//                return false;
+//            }
+        };
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        setSupportActionBar(toolbar);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new RecipeFragment(), "visible_fragment").commit();
+            navigationView.setCheckedItem(R.id.nav_recipes);
+            currentNavItem = R.id.nav_recipes;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Make sure that on resume the correct navigation item is checked
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        if (navigationView.getCheckedItem().getItemId() != currentNavItem) {
+            navigationView.setCheckedItem(currentNavItem);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_recipes:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new RecipeFragment(), "visible_fragment").commit();
+                break;
+            case R.id.nav_meal_plan:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MealPlanFragment(), "visible_fragment").commit();
+                break;
+            case R.id.nav_shopping:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ShoppingFragment(), "visible_fragment").commit();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.END);
+        return true;
+    }
+
+    // this changes the drawer button to one on the right side
+
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.END)) {
+            drawer.closeDrawer(GravityCompat.END);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
---
+++ app/src/main/java/com/example/foodforthought/RecipeFragment.java
@@ -0,0 +1,6 @@
+package com.example.foodforthought;
+
+import androidx.fragment.app.Fragment;
+
+public class RecipeFragment extends Fragment {
+}
